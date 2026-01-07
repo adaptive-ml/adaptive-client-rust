8828de384467656c555671008d2f9fdf15127594
yacineb <yacine@adaptive-ml.com>
2026-01-07 17:30:16 +0100

    🚧 grader config serializable

--- src/lib.rs
+++ src/lib.rs
@@ -268,7 +268,7 @@ pub struct GetRecipe;
 #[graphql(
     schema_path = "schema.gql",
     query_path = "src/graphql/grader.graphql",
-    response_derives = "Debug, Clone"
+    response_derives = "Debug, Clone, Serialize"
 )]
 pub struct GetGrader;
 
